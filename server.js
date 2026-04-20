a2ee7d73d6bea560f7b90c290106cddd3444f938
Elkin Aguirre <elkimaguirre.ea@gmail.com>
2026-04-20 13:51:06 -0500

    fix start script

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ app.use(express.json());
 
 // 🧱 Conexión MySQL
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: "metro.proxy.rlwy.net",
+  user: "root",
+  password: "BaioOseNTRHTzyJYbPAyCJHcSFMxMxyk",
+  database: "railway",
+  port: 10179
 });
 
 // Crear tabla si no existe
